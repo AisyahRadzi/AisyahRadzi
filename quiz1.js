73704ff793d7c04050753a26485191408e136148
unknown <aisyah000326@gmail.com>
2023-03-30 16:00:59 +0800

    C:\Users\User\Desktop\quiz1>

---
+++ quiz1.js
@@ -0,0 +1,50 @@
+let message = "benr"
+console.log(message,typeof message)
+
+message = 123
+console.log(message,typeof message)
+
+let str = "benr"
+let num = 1
+
+let total = parseFloat + num
+console.log(total,typeof total)
+
+
+
+
+let data = ["benr", 3523, true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+let user = {
+    name : "Aisyah",
+    age : 23,
+    faculty : "FKEKK"
+}
+
+console.log(user.name)
+console.log(user.age)
+console.log(user.faculty)
+
+
+let messages = [
+    {
+        name: "usr1",
+        date: "1/4/2023",
+        msg: "good video",
+        like: 1000
+    },
+
+    {
+        name: "usr2",
+        date: "1/4/2023",
+        msg: "very good video",
+        like: 4000
+    },
+]
+
+console.log(messages[1].like) //>>verygood video
